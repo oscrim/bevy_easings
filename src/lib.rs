df2c1b06004e8a3ce8a4bcd075b4efab760e1741
Oscar <oscar.rimsby@mobilaris.se>
2026-01-15 18:15:04 +0100

    Bevy 0.18

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,7 @@ pub use interpolation::EaseFunction;
 pub use interpolation::Lerp;
 
 mod plugin;
-pub use plugin::{EasingsLabel, EasingsPlugin, custom_ease_system};
+pub use plugin::{custom_ease_system, EasingsLabel, EasingsPlugin};
 mod implemented;
 
 /// Wrapper around a type that can be eased.
